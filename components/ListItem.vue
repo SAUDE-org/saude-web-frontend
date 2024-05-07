aea5155d35e7a950f674d23ef4138c09c94586ad
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-05-07 11:23:01 -0300

    feat: cria aquivo vue

---
+++ components/ListItem.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-full p-4 border b-gray-200 flex items-center justify-between">
+        <span class="text-xl font-semibold">
+            <slot />
+        </span>
+        <!-- Botões de Ação -->
+        <div class="w-32 space-x-2">
+            <slot name="actions" />
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- Listagem dos Contatos -->
+    <div class="w-full px-2">
+        <!-- Item Contato -->
+        <div class="w-full p-4 border b-gray-200 flex items-center justify-between">
+            <span class="text-xl font-semibold">João da Silva</span>
+            <!-- Botões de Ação -->
+            <div class="w-32 space-x-2">
+                <button>Editar</button>
+                <button>Excluir</button>
+            </div>
+        </div>
+    </div>
+
+    <button class="w-full p-4 mt-8 border b-blue-200 rounted bg-blue-500 text-white flex items-center justify-center">
+        Adicionar Contato
+    </button>
+</template>
