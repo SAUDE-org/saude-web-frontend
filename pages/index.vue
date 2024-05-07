0f730182a55d3da15c1d83c2d963968510f57f27
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-05-07 17:36:46 -0300

    feat: rotas 'login' e 'home'

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="w-full h-full">   
+        <h1>Login Page</h1>
+        <p>
+            <router-link :to="{ name: 'home' }">Home Page</router-link>
+        </p>
+    </div>
+</template>
