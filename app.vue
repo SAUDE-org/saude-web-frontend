b0dea6754615469c52acd9188856dfaa7cf904ae
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-05-07 11:22:11 -0300

    feat: tela home

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="w-full h-full">
+    <header class="w-full h-20 border b-gray-200 flex items-center pl-8">
+      <h1 class ="text-2xl font-bold">Contatinho.com</h1>
+    </header>
+
+    <main class="w-full h-full pt-4 px-8">
+      <NuxtPage />
+    </main>
   </div>
 </template>
