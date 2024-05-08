f9fdb6933e5a8492efca66df59e68d6406a83209
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-05-08 10:19:02 -0300

    fix: detalhe do roteamento

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <div class="w-full h-full">   
         <h1>Home Page</h1>
         <p>
-            <NuxtLink to="/Login">Home Page</NuxtLink>
+            <NuxtLink to="/login" target="_blank">Login Page</NuxtLink>
         </p>
     </div>
 </template>
