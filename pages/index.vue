8c6a8bce9f57f59bca671cc75eb9ee85fbcd0171
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-05-14 17:48:38 -0300

    feat: adiciona header

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,25 @@
 <template>
-    <div class="w-full h-full">   
+    <div id="home" class="w-full h-full">
+
+        <div id="header" class="w-full h-full"></div>
+        
+            <div id="contato" class="w-full border h-12 bg-teal-500 flex-items-center justify-btween">
+                <div>endereço</div>
+                <div>telefone</div>
+            </div>
+
+            <div id="menus" class="w-full borde h-12 bg-blue-200 flex-items-center justify-btween">
+                Menus
+            </div>
+
         <h1>Home Page</h1>
         <p>
             <NuxtLink to="/login" target="_blank">Login Page</NuxtLink>
         </p>
+
     </div>
 </template>
+
+<script setup>
+
+</script>
