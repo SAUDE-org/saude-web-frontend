1e242da9df86b2d32f518b3e3c407cc06bff2e1e
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-05-26 19:30:26 -0300

    feat: atualiza header

--- pages/index.vue
+++ pages/index.vue
@@ -7,17 +7,67 @@
     <div class="w-full h-full">
         <header>
             <div id="contato" class="w-full h-12 px-24 flex flex-row items-center justify-between" style="background-color: #07B4BA;">
-                <div id="endereco" class="">
-                    <div>endereço</div>
+                <div id="endereco" class="flex flex-row">
+                    <nav>
+                        <a href="
+                            https://www.google.com/maps/place/Praia+de+Ponta+Negra/@-5.8765216,-35.1769793,16.25z/data=!4m6!3m5!1s0x7b2ff2ae2c55695:0x3ba594c57ef58fc1!8m2!3d-5.8811012!4d-35.1629703!16zL20vMDlibXRf?entry=ttu
+                        ">
+                            <img src="assets\icon\vector.svg" alt="">                   
+                        </a>
+                    </nav>
+                    
+                    <span>Av. Praia, 40 - Ponta Negra - Natal/RN</span>
                 </div>
 
                 <div id="telefone" class="">
-                    <div>telefone</div>
+                    <nav class="flex flex-row items-center justify-between">
+                        <span>
+                            <a href="
+                                https://www.youtube.com/@metropoledigital
+                            ">
+                                <img src="assets\icon\youtube.svg" alt="">
+                            </a>
+                        </span>
+                        <span>
+                            <a href="
+                                https://www.facebook.com/MetropoleDigital/?locale=pt_BR
+                            ">
+                                <img src="assets\icon\facebook.svg" alt="">
+                            </a>
+                        </span>
+                        <span>                            
+                            <a href="
+                                https://br.linkedin.com/school/metropoledigital/
+                            ">
+                                <img src="assets\icon\linkedIn.svg" alt="">
+                            </a>
+                        </span>
+                        <span>                            
+                            <a href="
+                                https://www.instagram.com/metropoledigital/
+                            ">
+                                <img src="assets\icon\instagram.svg" alt="">
+                            </a>
+                        </span>
+                        <span>                            
+                            <a href="
+                                https://web.whatsapp.com/
+                            ">
+                                <img src="assets\icon\whatsapp.svg" alt="">
+                            </a>
+                        </span>
+                        <span>(84) 99999-9999</span>
+                    </nav>
                 </div>
             </div>
-            
-            <div id="logo" class="w-full h-12 px-24 flex flex-row items-center justify-between" style="background-color: #C5EBFE;">
-                Logomarca
+
+            <div id="logo" class="w-full h-12 px-24 flex flex-row items-center justify-start" style="background-color: #C5EBFE;">
+                <span>
+                    <img src="assets\icon\logo.svg" alt="Logo" width="50px" height="50px">
+                </span>
+                <span>
+                    <img src="assets\icon\text-logo.svg" alt="TextLogo" width="200px" height="200px">
+                </span>
             </div>        
         </header>
 
