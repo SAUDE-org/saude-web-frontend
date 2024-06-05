b69b6bfa97da9ae1f435ddad60a8e532351bae4b
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-06-04 23:41:45 -0300

    feat: cria tela cadastro de usuário

--- pages/cadastro.vue
+++ pages/cadastro.vue
@@ -1,5 +1,35 @@
 <template>
-    <div>
-        <h1>Cadastro de Usuário</h1>
-    </div>
+    <div class="w-full h-full">
+
+        <header class="flex justify-end">
+            <nav class="pr-32 pt-8">
+                <a href="index.vue">
+                    <img src="assets\icon\BotaoHome.svg" alt="">
+                </a>
+            </nav>
+        </header>
+
+        <main class="mx-24 mt-8 flex flex-col items-center">
+            
+            <!--box paciente-->
+            <div id="titCadastro" class="flex flex-row items-center justify-center">
+                <figure>
+                    <img src="assets\icon\tetoscope.svg" alt="">
+                </figure>
+                <span class="text-gray-500 italic">
+                    <h1>Cadastro de Usuário</h1>
+                </span>
+            </div>
+
+            <!--botão voltar-->
+            <nav class="mt-6 flex items-center justify-center">
+                <a href="/login">
+                    <button id="btnVoltar" type="button" class="w-60 btn btn-primary btn-lg"
+                        style="background-color: #76BFAC; border: 4px solid #CFE3E1; border-radius: 14px;">
+                        Voltar
+                    </button>
+                </a>
+            </nav>
+        </main>
+</div>
 </template>
