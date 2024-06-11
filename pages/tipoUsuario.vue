2fff2dde0f90231b26925e64ad43f988a91deb90
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-06-10 23:00:46 -0300

    feat: adiciona tela tipo de usuário

--- pages/tipoUsuario.vue
+++ pages/tipoUsuario.vue
@@ -1,5 +1,50 @@
+<script setup lang="ts">
+import BotaoSair from '~/components/BotaoSair.vue';
+</script>
+
 <template>
-    <div>
-        <h1>Tela Tipo Usuário</h1>
+    <div class="w-full h-full">
+
+        <header class="flex justify-end">
+            <BotaoSair />
+        </header>
+
+        <main class="mx-24 mt-4 flex flex-auto flex-col items-center">
+            
+            <!--box título-->
+            <div class="flex flex-row items-center justify-center">
+                <figure>
+                    <img src="assets\icon\tetoscope.svg" alt="">
+                </figure>
+                <span class="text-gray-500 italic">
+                    <h1>Qual o seu tipo de usuário?</h1>
+                </span>
+            </div>
+
+            <!--tipos de usuário-->
+            <div class="my-8 flex flex-row items-center justify-center">
+                <nav class="mx-12 flex grow">
+                    <a href="/cadastros/cadastroPaciente">
+                        <img src="assets\icon\paciente.svg" alt="">
+                    </a>
+                </nav>
+
+                <nav class="mx-12 flex grow">
+                    <a href="/cadastros/cadastroProfissional">
+                        <img src="assets\icon\profissional.svg" alt="">
+                    </a>
+                </nav>
+
+                <nav class="mx-12 flex grow">
+                    <a href="/cadastros/cadastroClinica">
+                        <img src="assets\icon\clínica.svg" alt="">
+                    </a>
+                </nav>               
+            </div>            
+
+
+
+
+        </main>
     </div>
 </template>
