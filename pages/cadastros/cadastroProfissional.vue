2d36d883c8b71a5ece03300ef2afffd15088b6fc
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-06-10 23:20:14 -0300

    feat: adiciona tela cadastro de profissional

--- pages/cadastros/cadastroProfissional.vue
+++ pages/cadastros/cadastroProfissional.vue
@@ -1,7 +1,108 @@
 <template>
-    <div>
-        <h1>
-            Cadastro do Profissional
-        </h1>
+    <div class="w-full h-full">
+
+        <header class="flex justify-end">
+            <BotaoSair />
+        </header>
+
+        <main class="mx-24 mt-8 flex flex-col items-center">
+            
+            <!--box título-->
+            <div id="titCadPaciente" class="mb-8 flex flex-row items-center justify-center">
+                <span class="text-gray-500 italic">
+                    <h1>Cadastro de Profissional</h1>
+                </span>
+            </div>
+
+                <!--box input texto nome-->
+                <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch"
+                    style="background-color: #ecf3f1; border-radius: 14px;">
+                    <div class="p-2 italic text-slate-600">
+                        Nome
+                    </div>
+                    <div class="grow">
+                        <input class="form-control fw-lighter" id="nome" type="text" placeholder="Digite seu nome aqui"
+                            style="background-color: #ecf3f1; border-color: #ecf3f1;">
+                    </div>
+                </div>
+
+                <!--box input texto cpf-->
+                <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch"
+                    style="background-color: #ecf3f1; border-radius: 14px;">
+                    <div class="p-2 italic text-slate-600">
+                        CPF
+                    </div>
+                    <div class="grow">
+                        <input class="form-control fw-lighter" id="cpf" type="text" placeholder="Digite seu cpf aqui"
+                            style="background-color: #ecf3f1; border-color: #ecf3f1;">
+                    </div>
+                </div>
+
+                <!--box input texto registro-->
+                <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch"
+                    style="background-color: #ecf3f1; border-radius: 14px;">
+                    <div class="p-2 italic text-slate-600">
+                        Registro
+                    </div>
+                    <div class="grow">
+                        <input class="form-control fw-lighter" id="registro" type="text" placeholder="Digite seu registro profissional aqui"
+                            style="background-color: #ecf3f1; border-color: #ecf3f1;">
+                    </div>
+                </div>
+
+                <!--box input texto email-->
+                <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch"
+                    style="background-color: #ecf3f1; border-radius: 14px;">
+                    <div class="p-2 italic text-slate-600">
+                        E-mail
+                    </div>
+                    <div class="grow">
+                        <input class="form-control fw-lighter" id="email" type="text" placeholder="Digite seu email aqui"
+                            style="background-color: #ecf3f1; border-color: #ecf3f1;">
+                    </div>
+                </div>
+
+                <!--box input texto telefone-->
+                <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch"
+                    style="background-color: #ecf3f1; border-radius: 14px;">
+                    <div class="p-2 italic text-slate-600">
+                        Telefone
+                    </div>
+                    <div class="grow">
+                        <input class="form-control fw-lighter" id="telefone" type="text" placeholder="Digite neste formato: (99) 99999-9999"
+                            style="background-color: #ecf3f1; border-color: #ecf3f1;">
+                    </div>
+                </div>
+
+                <!--box input texto endereço-->
+                <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch"
+                    style="background-color: #ecf3f1; border-radius: 14px;">
+                    <div class="p-2 italic text-slate-600">
+                        Endereço
+                    </div>
+                    <div class="grow">
+                        <input class="form-control fw-lighter" id="endereco" type="text" placeholder="Digite seu endereço completo aqui"
+                            style="background-color: #ecf3f1; border-color: #ecf3f1;">
+                    </div>
+                </div>
+
+            <br>
+            <!--botão cadastrar-->
+            <nav>
+                <a href="/login">
+                    <button id="btn-cadastrar" onclick="" type="button" class="btn">
+                        Cadastrar
+                    </button>
+                </a>
+            </nav>
+
+            <!-- Caixa de alerta personalizada -->
+            <div id="alertBox"></div>
+
+        </main>
     </div>
 </template>
+
+<style>
+    @import url("~/assets/css/btn.css");
+</style>
