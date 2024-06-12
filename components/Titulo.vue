4f1aab5fb6f1e2500fe8dcff95e37ca6b6bfd142
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-06-12 14:47:55 -0300

    feat-date: cria tabela de dados

--- components/Titulo.vue
+++ components/Titulo.vue
@@ -1,5 +1,5 @@
 <template>
-    <span class="flex flex-col items-center">
+    <span class="my-2 flex flex-col items-center">
         <slot />
     </span>
 </template>
--- pages/sig/paciente.vue
+++ pages/sig/paciente.vue
@@ -14,18 +14,35 @@
 
         <main class="mx-24 mt-8 flex flex-col items-center">
 
-            <div id="data-atual">
-                QUA, 12 Junho 2024
+            <div id="data-atual" class="mb-8 text-xl font-semibold text-black italic">
+                QUI, 12 Junho 2024
             </div>
 
-            <div>
-                <span class="text-black">
+            <div class="mb-8 text-xl font-semibold text-black">
                     Sua próxima consulta agendada é:
-                </span>
             </div>
 
-            <div id="prox-consulta" style="border: solid; border-radius: 14px; border-color: aqua;">
-                <span>Nutricionista</span>
+            <div id="prox-consulta" style="border: solid; border-radius: 14px; border-color: #39D0CB;">
+                <div class="p-2 flex flex-row items-start">
+
+                    <div class="pr-4">
+                        <div id="profissional" class="flex flex-col items-start">
+                            <span class="text-xl font-semibold">Nutricionista</span>
+                            <span>Dra. Ana Maria Braga</span>
+                        </div>
+
+                        <div id="data-consulta" class="mt-4 flex flex-col items-start">
+                            <span>Seg., 22 de ago. de 2024</span>
+                            <span id="data-consulta">11:30h</span>
+                        </div>
+                    </div>
+
+                    <div id="local-consulta" class="flex flex-col items-end">
+                        <span class="text-xl font-semibold">Hospital Ponta Negra</span>
+                        <span>Av. Ayrton Senna, 34 - Capim Macio</span>
+                    </div>
+
+                </div>
             </div>
 
             <Titulo />
