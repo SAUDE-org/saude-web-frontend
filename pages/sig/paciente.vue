c99177a789951f6f66f0ad91a0b916681f1c63f1
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-06-12 12:32:46 -0300

    fix: remove comentários

--- pages/sig/paciente.vue
+++ pages/sig/paciente.vue
@@ -21,23 +21,12 @@
             <div>
                 <span class="text-black">
                     Sua próxima consulta agendada é:
-                </span>                
+                </span>
             </div>
 
             <div id="prox-consulta" style="border: solid; border-radius: 14px; border-color: aqua;">
                 <span>Nutricionista</span>
             </div>
-
-<!--
-    <span style="color: #506175; font-size: 32px; font-style: italic; flex: auto; justify-content: center;">            
--->
-            
-        <!--
-            <span class="flex flex-col items-center" style="color: #506175; font-size: 32px; font-style: italic;">
-                <div>Você quer agendar uma</div>                
-                <div>consulta ou ver seus exames?</div>
-            </span>
-        -->
 
             <Titulo />
             <div id="titulo">Você quer agendar uma</div>
