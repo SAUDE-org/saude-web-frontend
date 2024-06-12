1838301392207d8c641fb00e2b409de5e6e6d87d
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-06-12 00:24:41 -0300

    feat-header: adiciona perfil

--- pages/sig/paciente.vue
+++ pages/sig/paciente.vue
@@ -3,11 +3,12 @@
 
         <header>
 
-            <div class="w-full h-20 flex justify-end" style="background-color:  #d9d9d9;" >
-                
+            <div class="w-full h-20 flex flex-row items-center justify-end" style="background-color:  #d9d9d9;" >
+
+                <img src="assets/icon/profile_icon.svg" alt="Perfil">
+                <div class="font-serif text-3xl">Sr.(a) João Maria de José</div>
                 <BotaoSair />
                 
-
             </div>
 
             <div>
