cf8f8e3bb11f85d55a8fcd876cb3ad74a9d6ca5e
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-06-12 00:26:41 -0300

    fix-btn: ajusta botão/component sair

--- components/BotaoSair.vue
+++ components/BotaoSair.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="pr-32">
+    <nav class="pr-32 ml-8">
         <a href="http://localhost:3000/">
             <img src="assets\icon\BotaoSair.svg" alt="Botão Sair">
         </a>
