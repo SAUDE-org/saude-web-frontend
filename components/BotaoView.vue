9c149aa44ee1dbcf952b41a4447d596bd2e5bc86
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-06-13 02:05:48 -0300

    feat-components: cria btn/component BotaoView

---
+++ components/BotaoView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="mt-3 flex flex-col items-end">
+        <button class="btn-view">
+            <slot />
+        </button>
+    </div>
+</template>
+
+<style>
+    @import url("~/assets/css/btn.css");
+</style>
