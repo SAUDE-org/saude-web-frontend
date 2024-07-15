bd1989376bfd8ef93cf2599b8565856e4ba21e84
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-15 20:34:32 -0300

    feat-config: cria classe WebConfig para permitir acesso a aPI de qualquer lugar

--- pages/cadastro/paciente.vue
+++ pages/cadastro/paciente.vue
@@ -14,51 +14,51 @@
                 </span>
             </div>
 
-                <!--box input texto nome-->
-                <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch" style="background-color: #ecf3f1; border-radius: 25px;">
-                    <div class="p-2 italic text-slate-600">Nome</div>
-                    <div class="grow">
-                        <input v-model="nome" class="form-control fw-lighter" id="nome" type="text" placeholder="Digite seu nome aqui" style="background-color: #ecf3f1; border-color: #ecf3f1;">
-                    </div>
+            <!-- box input texto nome -->
+            <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch" style="background-color: #ecf3f1; border-radius: 25px;">
+                <div class="p-2 italic text-slate-600">Nome</div>
+                <div class="grow">
+                <input v-model="nome" class="form-control fw-lighter" id="nome" type="text" placeholder="Digite seu nome aqui" style="background-color: #ecf3f1; border-color: #ecf3f1;">
                 </div>
+            </div>
 
-                <!--box input texto cpf-->
-                <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch" style="background-color: #ecf3f1; border-radius: 25px;">
-                    <div class="p-2 italic text-slate-600">CPF</div>
-                    <div class="grow">
-                        <input v-model="cpf" class="form-control fw-lighter" id="cpf" type="text" placeholder="Digite seu cpf aqui" style="background-color: #ecf3f1; border-color: #ecf3f1;">
-                    </div>
+            <!-- box input texto cpf -->
+            <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch" style="background-color: #ecf3f1; border-radius: 25px;">
+                <div class="p-2 italic text-slate-600">CPF</div>
+                <div class="grow">
+                <input v-model="cpf" class="form-control fw-lighter" id="cpf" type="text" placeholder="Digite seu cpf aqui" style="background-color: #ecf3f1; border-color: #ecf3f1;">
                 </div>
+            </div>
 
-                <!--box input texto email-->
-                <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch" style="background-color: #ecf3f1; border-radius: 25px;">
-                    <div class="p-2 italic text-slate-600">E-mail</div>
-                    <div class="grow">
-                        <input v-model="email" class="form-control fw-lighter" id="email" type="text" placeholder="Digite seu email aqui" style="background-color: #ecf3f1; border-color: #ecf3f1;">
-                    </div>
+            <!-- box input texto email -->
+            <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch" style="background-color: #ecf3f1; border-radius: 25px;">
+                <div class="p-2 italic text-slate-600">E-mail</div>
+                <div class="grow">
+                <input v-model="email" class="form-control fw-lighter" id="email" type="text" placeholder="Digite seu email aqui" style="background-color: #ecf3f1; border-color: #ecf3f1;">
                 </div>
+            </div>
 
-                <!--box input texto telefone-->
-                <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch" style="background-color: #ecf3f1; border-radius: 25px;">
-                    <div class="p-2 italic text-slate-600">Telefone</div>
-                    <div class="grow">
-                        <input v-model="telefone" class="form-control fw-lighter" id="telefone" type="text" placeholder="Digite neste formato: (99) 99999-9999" style="background-color: #ecf3f1; border-color: #ecf3f1;">
-                    </div>
+            <!-- box input texto telefone -->
+            <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch" style="background-color: #ecf3f1; border-radius: 25px;">
+                <div class="p-2 italic text-slate-600">Telefone</div>
+                <div class="grow">
+                <input v-model="telefone" class="form-control fw-lighter" id="telefone" type="text" placeholder="Digite neste formato: (99) 99999-9999" style="background-color: #ecf3f1; border-color: #ecf3f1;">
                 </div>
+            </div>
 
-                <!--box input texto endereço-->
-                <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch" style="background-color: #ecf3f1; border-radius: 25px;">
-                    <div class="p-2 italic text-slate-600">Endereço</div>
-                    <div class="grow">
-                        <input v-model="endereco" class="form-control fw-lighter" id="endereco" type="text" placeholder="Digite seu endereço completo aqui" style="background-color: #ecf3f1; border-color: #ecf3f1;">
-                    </div>
+            <!-- box input texto endereço -->
+            <div class="w-25 h-14 mb-2 flex flex-row items-center justify-items-stretch" style="background-color: #ecf3f1; border-radius: 25px;">
+                <div class="p-2 italic text-slate-600">Endereço</div>
+                <div class="grow">
+                <input v-model="endereco" class="form-control fw-lighter" id="endereco" type="text" placeholder="Digite seu endereço completo aqui" style="background-color: #ecf3f1; border-color: #ecf3f1;">
                 </div>
+            </div>
 
             <!-- Botão cadastrar -->
             <br>
             <nav>
                 <button @click="cadastrar" type="button" class="btn">Cadastrar</button>
-            </nav>
+            </nav>                
 
             <!-- Caixa de alerta personalizada -->
             <div id="alertBox"></div>
@@ -78,36 +78,42 @@ const telefone = ref('');
 const endereco = ref('');
 
 const cadastrar = async () => {
+  try {
     const { data, error } = await useFetch('http://localhost:8080/paciente/inserir', {
-        method: 'POST',
-        body: {
-            nome: nome.value,
-            cpf: cpf.value,
-            email: email.value,
-            telefone: telefone.value,
-            endereco: endereco.value,
-        }
+      method: 'POST',
+      body: {
+        nome: nome.value,
+        cpf: cpf.value,
+        email: email.value,
+        telefone: telefone.value,
+        endereco: endereco.value,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
 
-    if (error.value) {
-        alert('Erro ao cadastrar paciente: ' + error.value.message);
+    if (error) {
+      alert('Erro ao cadastrar paciente: ' + error.message);
     } else {
-        alert('Paciente cadastrado com sucesso!');
+      alert('Paciente cadastrado com sucesso!');
 
-        // Limpar os dados dos inputs
-        nome.value = '';
-        cpf.value = '';
-        email.value = '';
-        telefone.value = '';
-        endereco.value = '';
+      // Limpar os dados dos inputs
+      nome.value = '';
+      cpf.value = '';
+      email.value = '';
+      telefone.value = '';
+      endereco.value = '';
     }
+  } catch (error) {
+    console.error('Erro ao fazer login:', error);
+    alert('Erro ao cadastrar paciente');
+  }
 };
 
 import BotaoSair from '~/components/BotaoSair.vue';
 
-    /**
-    @import url("~/assets/js/btn.js");
-     */
+/** @import url("~/assets/js/btn.js"); */
 </script>
 
 <style>
